26c32c0c19f4f6927e34383a45c0bed27fb71f75
nonokaM <c1201071@gmail.com>
2023-02-20 05:27:51 +0900

    MyTimer完成

--- MyTimer/MyTimer/ContentView.swift
+++ MyTimer/MyTimer/ContentView.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var timerHandler : Timer?
+    @State var count = 0
+    @AppStorage("timer_value") var timerValue = 10
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("タイマー画面")
+            
+            ZStack {
+                Image("backgroundTimer")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFill()
+                
+                VStack(spacing: 30.0) {
+                    Text("残り\(timerValue - count)秒")
+                        .font(.largeTitle)
+                    
+                    HStack {
+                        Button {
+                            startTimer()
+                        } label: {
+                            Text("スタート")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("startColor"))
+                                .clipShape(Circle())
+                        }
+                        
+                        Button {
+                            if let unwrapedTimerHandler = timerHandler {
+                                if unwrapedTimerHandler.isValid == true {
+                                    unwrapedTimerHandler.invalidate()
+                                }
+                            }
+                        } label: {
+                            Text("ストップ")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("stopColor"))
+                                .clipShape(Circle())
+                        }
+                    }
+                }
             }
+            .onAppear {
+                count = 0
+            }
+
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     NavigationLink {
@@ -23,7 +69,26 @@ struct ContentView: View {
                 }
             }
         }
-
+    }
+    
+    func countDownTimer() {
+        count += 1
+        if timerValue - count <= 0 {
+            timerHandler?.invalidate()
+        }
+    }
+    func startTimer() {
+        if let unwrapedTimerHandler = timerHandler {
+            if unwrapedTimerHandler.isValid == true {
+                return
+            }
+        }
+        if timerValue - count <= 0 {
+            count = 0
+        }
+        timerHandler = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {
+             _ in countDownTimer()
+        }
     }
 }
 
