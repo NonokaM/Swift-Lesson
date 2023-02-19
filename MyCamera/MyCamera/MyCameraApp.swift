8f6bea6e2d5a5d72b8e9e6c509fdce27e298fb9b
nonokaM <c1201071@gmail.com>
2023-02-20 07:22:01 +0900

    MyCamera完成

---
+++ MyCamera/MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by MOMII Nonoka on 2023/02/20.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
