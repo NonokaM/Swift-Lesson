e3b2cc6f9b2c346c1a35f64952aac2a355251a52
nonokaM <c1201071@gmail.com>
2023-02-19 17:38:54 +0900

    じゃんけけんアプリのファイル準備

---
+++ MyJanken/MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by MOMII Nonoka on 2023/02/19.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
