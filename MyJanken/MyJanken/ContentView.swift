4dec8a1a3cdaca2385c1ea1765de9ffa19bcd775
nonokaM <c1201071@gmail.com>
2023-02-19 18:46:11 +0900

    ランダムにじゃんけん

--- MyJanken/MyJanken/ContentView.swift
+++ MyJanken/MyJanken/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var answerNumber = 0
+
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            if answerNumber == 0 {
+                Text("これからじゃんけんをします！")
+            } else if answerNumber == 1 {
+                // グー画像を表示
+                Image("gu")
+                // リサイズ指定
+                    .resizable()
+                // アスペクト比（縦横比）を維持する指定
+                    .scaledToFit()
+                Text("グー")
+                
+            } else if answerNumber == 2 {
+                Image("choki")
+                    .resizable()
+                    .scaledToFit()
+                Text("チョキ")
+
+            } else {
+                Image("pa")
+                    .resizable()
+                    .scaledToFit()
+                Text("パー")
+            }
+
+
+            Button {
+                answerNumber = Int.random(in: 1...3)
+            } label: {
+                Text("じゃんけんをする！")
+            }
+
         }
-        .padding()
     }
 }
 
