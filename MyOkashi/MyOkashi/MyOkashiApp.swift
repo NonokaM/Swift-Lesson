15ca6b02d52c7b656134e79f2603ad996caf0524
nonokaM <c1201071@gmail.com>
2023-02-20 16:29:44 +0900

    MyOkashi完成

---
+++ MyOkashi/MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by MOMII Nonoka on 2023/02/20.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
