e7d61ee4a66ea2f11bc3510845cbb537bda7f0f6
nonokaM <c1201071@gmail.com>
2023-02-24 23:22:19 +0900

    メモアプリを作成

---
+++ SimpleMemo/SimpleMemo/SimpleMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMemoApp.swift
+//  SimpleMemo
+//
+//  Created by MOMII Nonoka on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
